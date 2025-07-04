4e2b3ed6bacea84ddae07f2545917e623fc74460
dewmal <dewmal@syigen.com>
2025-07-04 23:07:08 +0530

    Finish batch related cases

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+use std::fmt::Display;
 use std::io;
+use std::str::FromStr;
 
+#[repr(u8)]
+#[derive(Debug, Clone)]
+enum BatchStatus {
+    Active = 1,
+    Inactive = 0,
+}
+#[derive(Debug, PartialEq, Eq)]
+struct ParseBatchStatusError;
+impl FromStr for BatchStatus {
+    type Err = ParseBatchStatusError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim() {
+            "1" => Ok(BatchStatus::Active),
+            "0" => Ok(BatchStatus::Inactive),
+            _ => Err(ParseBatchStatusError),
+        }
+    }
+}
+
+impl Display for BatchStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            BatchStatus::Active => write!(f, "Active"),
+            BatchStatus::Inactive => write!(f, "Inactive"),
+        }
+    }
+}
 #[derive(Debug, Clone)]
 struct Batch {
     name: String,
-    status: String,
+    status: BatchStatus,
 }
 
 fn main() {
@@ -34,7 +63,16 @@ fn main() {
                     "2" => add_batch(&mut batch_details),
                     _ => (),
                 },
-                "2" => {}
+                "2" => match main_choice.as_str() {
+                    "1" => println!("Update Student"),
+                    "2" => update_batch(&mut batch_details),
+                    _ => (),
+                },
+                "3" => match main_choice.as_str() {
+                    "1" => println!("View Student Profile"),
+                    "2" => view_batches(&batch_details),
+                    _ => (),
+                },
                 _ => (),
             }
         }
@@ -57,7 +95,10 @@ fn add_batch(batch_details: &mut Vec<Batch>) {
 
     let new_batch = Batch {
         name: batch_name,
-        status: batch_status,
+        status: batch_status
+            .trim()
+            .parse()
+            .expect("Failed to parse batch status"),
     };
 
     batch_details.push(new_batch);
@@ -71,6 +112,42 @@ fn add_batch(batch_details: &mut Vec<Batch>) {
 
     if choice.trim() == "y" {
         add_batch(batch_details);
+    }
+}
+
+fn update_batch(batch_details: &mut Vec<Batch>) {
+    let mut batch_name = String::new();
+    let mut batch_status = String::new();
+
+    println!("Enter batch Name : ");
+    io::stdin()
+        .read_line(&mut batch_name)
+        .expect("Failed to read line");
+
+    println!("Enter batch Status : ");
+    io::stdin()
+        .read_line(&mut batch_status)
+        .expect("Failed to read line");
+
+    let new_batch = Batch {
+        name: batch_name,
+        status: batch_status.parse().expect("Failed to parse batch status"),
+    };
+
+    for batch in batch_details.iter_mut() {
+        if batch.name == new_batch.name {
+            *batch = new_batch.clone();
+            println!("Batch updated successfully");
+            return;
+        }
+    }
+
+    println!("Batch not found");
+}
+
+fn view_batches(batch_details: &Vec<Batch>) {
+    for batch in batch_details {
+        println!("Batch Name: {} / Batch Status {}", batch.name, batch.status);
     }
 }
 
