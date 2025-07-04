f1c8fe732ec7dc0bdde43331130f8e8d4087ca96
dewmal <dewmal@syigen.com>
2025-07-04 22:44:24 +0530

    Fix menu

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,93 @@
 use std::io;
 
+#[derive(Debug, Clone)]
+struct Batch {
+    name: String,
+    status: String,
+}
+
+fn main() {
+    let mut batch_details: Vec<Batch> = vec![];
+
+    // Main Menu Loop
+    loop {
+        let main_choice = print_main_menu();
+        if main_choice == "EXIT" {
+            break;
+        }
+
+        println!("Choice: {} {}", main_choice, main_choice.as_str() == "1");
+        // Sub Menu Loop
+        loop {
+            let sub_choice = match main_choice.as_str() {
+                "1" => print_student_menu(),
+                "2" => print_batch_menu(),
+                _ => "EXIT".to_string(),
+            };
+            if sub_choice == "EXIT" {
+                break;
+            }
+
+            match sub_choice.trim() {
+                "1" => match main_choice.as_str() {
+                    "1" => add_student(&mut batch_details),
+                    "2" => add_batch(&mut batch_details),
+                    _ => (),
+                },
+                "2" => {}
+                _ => (),
+            }
+        }
+    }
+}
+
+fn add_batch(batch_details: &mut Vec<Batch>) {
+    let mut batch_name = String::new();
+    let mut batch_status = String::new();
+
+    println!("Enter batch Name : ");
+    io::stdin()
+        .read_line(&mut batch_name)
+        .expect("Failed to read line");
+
+    println!("Enter batch Status : ");
+    io::stdin()
+        .read_line(&mut batch_status)
+        .expect("Failed to read line");
+
+    let new_batch = Batch {
+        name: batch_name,
+        status: batch_status,
+    };
+
+    batch_details.push(new_batch);
+    println!("Batch added successfully");
+
+    println!("Do you want to add another batch? (y/n): ");
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    if choice.trim() == "y" {
+        add_batch(batch_details);
+    }
+}
+
+fn add_student(batch_details: &mut Vec<Batch>) {
+    let mut batch_name = String::new();
+    let batch_status = String::new();
+    let student_name = String::new();
+    let student_id = String::new();
+    let student_email = String::new();
+    let student_phone = String::new();
+
+    println!("Enter batch Number (Students should be added) : ");
+    io::stdin()
+        .read_line(&mut batch_name)
+        .expect("Failed to read line");
+}
+
 fn print_main_menu() -> String {
     let header_count = 41;
     for _ in 0..header_count {
@@ -29,8 +117,10 @@ fn print_main_menu() -> String {
     io::stdin()
         .read_line(&mut choice)
         .expect("Failed to read line");
-
-    choice
+    if choice.trim() == "5" {
+        return "EXIT".to_string();
+    }
+    choice.trim().to_string()
 }
 
 fn print_student_menu() -> String {
@@ -38,7 +128,7 @@ fn print_student_menu() -> String {
     for _ in 0..header_count {
         print!("-");
     }
-    println!("\n|\tiCET STUDENT MANAGEMENT SYSTEM\t|");
+    println!("\n|\tStudent Management\t|");
 
     for _ in 0..header_count {
         print!("-");
@@ -63,35 +153,40 @@ fn print_student_menu() -> String {
         .read_line(&mut choice)
         .expect("Failed to read line");
 
+    if choice.trim() == "5" {
+        return "EXIT".to_string();
+    }
+
     choice
 }
 
-fn main() {
-    let choice = print_main_menu();
-    let sub_choice = match choice.trim().parse::<usize>() {
-        Ok(choice) => match choice {
-            1 => print_student_menu(),
-            // 2 => println!("You selected Batch Management"),
-            // 3 => println!("You selected Grade Management"),
-            // 4 => println!("You selected Report Generation"),
-            // 5 => println!("You selected Exit"),
-            _ => "-1".to_string(),
-        },
-        Err(e) => {
-            println!("Invalid input: {}", e);
-            "-1".to_string()
-        }
-    };
-
-    match sub_choice.trim().parse::<usize>() {
-        Ok(choice) => match choice {
-            1 => println!("You selected Add Student"),
-            // 2 => println!("You selected Update Student"),
-            // 3 => println!("You selected Delete Student"),
-            // 4 => println!("You selected Display Student"),
-            // 5 => println!("You selected Exit"),
-            _ => println!("Invalid choice"),
-        },
-        Err(e) => println!("Invalid input: {}", e),
+fn print_batch_menu() -> String {
+    let header_count = 41;
+    for _ in 0..header_count {
+        print!("-");
     }
+    println!("\n|\tBatch Management\t|");
+
+    for _ in 0..header_count {
+        print!("-");
+    }
+
+    let base_commands = vec!["Add Batch", "Update Batch", "View Batches", "Exit"];
+    println!();
+    for (index, command) in base_commands.iter().enumerate() {
+        println!("[{}] {}", index + 1, command);
+    }
+
+    println!("Enter your choice: ");
+
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    if choice.trim() == "4" {
+        return "EXIT".to_string();
+    }
+
+    choice
 }
